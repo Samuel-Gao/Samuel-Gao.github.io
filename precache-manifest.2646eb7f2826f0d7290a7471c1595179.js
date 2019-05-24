7acf205cbdb2dd77685cf7418a46442ade7d692e
Samuel Gao <samuelgao@Samuels-MacBook-Pro.local>
2019-05-23 23:51:22 -0400

    Updates

---
+++ precache-manifest.2646eb7f2826f0d7290a7471c1595179.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6fe63faf932cc396dbfacaa31983dce",
+    "url": "/static/media/flask.a6fe63fa.svg"
+  },
+  {
+    "revision": "4f0ee827e6c8f7d28785",
+    "url": "/static/css/main.a466eaf2.chunk.css"
+  },
+  {
+    "revision": "2319c3dc7170bb33b6b6",
+    "url": "/static/js/1.2319c3dc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "faaaa24690848901f08884d3d5a3584c",
+    "url": "/static/media/gmail.faaaa246.svg"
+  },
+  {
+    "revision": "5a19363e33ae9dce56dbc5822bd3c59f",
+    "url": "/static/media/reuse.5a19363e.svg"
+  },
+  {
+    "revision": "4f0ee827e6c8f7d28785",
+    "url": "/static/js/main.4f0ee827.chunk.js"
+  },
+  {
+    "revision": "d845e640fe84b26b5b6a53af5b5f2423",
+    "url": "/static/media/responsive.d845e640.svg"
+  },
+  {
+    "revision": "c98753bf3fbccb430ca23d7f9d27fcc8",
+    "url": "/static/media/vizlly.c98753bf.png"
+  },
+  {
+    "revision": "6cb3d030873809e401f87697f6fd621a",
+    "url": "/static/media/retreat.6cb3d030.png"
+  },
+  {
+    "revision": "320bf9a06a4d452e3d533644c0370eb3",
+    "url": "/static/media/caseware.320bf9a0.png"
+  },
+  {
+    "revision": "b5fd2410b2d5e68d55703de90bb722be",
+    "url": "/index.html"
+  }
+];
